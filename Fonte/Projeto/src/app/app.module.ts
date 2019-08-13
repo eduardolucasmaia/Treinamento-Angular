b51ffbb60bb43708227e67cc79af9adec1b9c810
Eduardo Maia <eduardolucasmaia@hotmail.com>
2019-08-13 20:16:49 -0300

    Treinamento do material aula 1 a 10 do curso
    Finalização do das aulas 1 a 10 do curso de Angular.

--- Fonte/Projeto/src/app/app.module.ts
+++ Fonte/Projeto/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './myfirst.component';
+import { MySecondComponent } from './mysecond/mysecond.component';
+import { MyThirdComponent } from './mysecond/mythird/mythird.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent,
+    MySecondComponent,
+    MyThirdComponent
   ],
   imports: [
     BrowserModule
---
+++ Fonte/Projeto/src/app/mysecond/mysecond.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mysecond',
+  templateUrl: './mysecond.component.html',
+  styleUrls: ['./mysecond.component.css']
+})
+export class MySecondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
