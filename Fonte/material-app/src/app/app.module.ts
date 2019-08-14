32acbf28d217e9c3ebebe4294e33c7b60aded416
Eduardo Maia <eduardolucasmaia@hotmail.com>
2019-08-13 21:30:43 -0300

    Inicio aulas com string interpolation

--- Fonte/material-app/src/app/app.module.ts
+++ Fonte/material-app/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringInterpolationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
